91b3f011aae6e7de574979196e833dae1247707d
Nitin Singh <nitin99bop2codf@gmail.com>
2021-10-03 19:24:35 +0530

    bug pix

--- app/src/main/java/com/example/paynav_assignment/Activities/MainActivity.java
+++ app/src/main/java/com/example/paynav_assignment/Activities/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomsheet_state = true;
+
         initalize();
 
         return_receive_veiwpager();
